72df288f90fe259e8f73c475266183e34827b184
Fernando Gatto <fernandogatto17@gmail.com>
2020-01-27 23:35:34 -0300

    Adicionado arquivos js de mascara

---
+++ js/states-city-dynamic.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    $.getJSON('/js/json/estados_cidades.json', function (data) {
+        var items = [];
+        var options = '<option value="">Estado*</option>';
+        $.each(data, function (key, val) {
+            options += '<option value="' + val.nome + '">' + val.nome + '</option>';
+        });
+        $("#estados").html(options);
+
+        $("#estados").change(function () {
+
+            var options_cidades = '';
+            var str = "";
+
+            $("#estados option:selected").each(function () {
+                str += $(this).text();
+            });
+
+            $.each(data, function (key, val) {
+                if (val.nome == str) {
+                    $.each(val.cidades, function (key_city, val_city) {
+                        options_cidades += '<option value="' + val_city + '">' + val_city + '</option>';
+                    });
+                }
+            });
+            $("#cidades").html(options_cidades);
+
+        }).change();
+
+    });
+
+});
