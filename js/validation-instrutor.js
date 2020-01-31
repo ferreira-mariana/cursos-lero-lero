f105d4ad91d02e80e4c14a26cb7419f7871d26e2
Mariana Ferreira <mariana.f.freitas@accenture.com>
2020-01-31 20:39:04 -0300

    validacao instrutor

--- js/validation-instrutor.js
+++ js/validation-instrutor.js
@@ -3,30 +3,33 @@ $(document).ready(function () {
   $(function () {
     $("form[name='novoinstrutor']").validate({
       rules: {
-        nome: {required: true, minlength: 3},
-        email: {required: true, minlength: 5},
-        valor_hora: "required",
-        login: {required: true, minlength: 5},
-        senha: {required: true, minlength: 5},
+        nome: {required: true, minlength: 3, maxlength: 50},
+        email: {required: true, minlength: 5, maxlength: 50},
+        login: {required: true, minlength: 5, maxlength: 20},
+        senha: {required: true, minlength: 5, maxlength: 255},
       },
       messages: {
         nome: {
           required: "Por favor, digite o nome do instrutor.",
-          minlength: "O nome deve ter pelo menos três caracteres."
+          minlength: "O nome deve ter pelo menos três caracteres.",
+          maxlength: "O nome deve ter no máximo 50 caracteres.",
         },
         email: {
           required: "Por favor, digite o email do instrutor.",
-          minlength: "O email deve ter pelo menos cinco caracteres."
+          minlength: "O email deve ter pelo menos cinco caracteres.",
+          maxlength: "O email deve ter no máximo 50 caracteres.",
         },
         login: {
           required: "Por favor, digite o seu nome para login.",
-          minlength: "O login deve ter pelo menos cinco caracteres."
+          minlength: "O login deve ter pelo menos cinco caracteres.",
+          maxlength: "O login deve ter no máximo 20 caracteres.",
         },
         senha: {
           required: "Por favor, digite a seu senha.",
-          minlength: "A senha deve ter pelo menos cinco caracteres."
+          minlength: "A senha deve ter pelo menos cinco caracteres.",
+          maxlength: "A senha deve ter no máximo 255 caracteres.",
+
         },
-        valor_hora: "Por favor, digite o valor da hora.",
       },
       submitHandler: function (form) {
         form.submit();
